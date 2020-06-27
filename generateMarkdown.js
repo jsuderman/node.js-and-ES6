d93531c8f2e60f82318ea28309e915e401ff81c5
Justin Suderman <jsuderman1@gmail.com>
2020-06-27 19:23:35 -0400

    added new file - generate md

---
+++ generateMarkdown.js
@@ -0,0 +1,30 @@
+// function to generate markdown for README
+function generateMarkdown(data) {
+    return `# ${data.project}
+    ## Description 
+    ${data.description.response}
+    ## table of contents
+    Installation
+    Usage
+    License
+    Contributing
+    Tests
+    Questions
+    ## ${data.installations}
+    ${data.installations.response}
+    ## ${data.usage}
+    ${data.usage.response}
+    ## License
+    ${data.license.response}
+    ## Contributing
+    ${data.contirbution.response}
+    ## Test
+    ${data.tests.response}
+    ## Questions
+    Any questions? please find me at [here](https://github.com/${data.github.response})
+  
+  `;
+  }
+  
+  module.exports = generateMarkdown;
+  
--- index.js
+++ index.js
@@ -4,9 +4,8 @@ const util = require("util");
 
 const writeFileAsync = util.promisify(fs.writeFile);
 // questions for users to answer to fill in a dynamically generated readme file
-// function promptUser() {
-//     return 
-    inquirer.prompt([
+const questions = [
+
         {
             type: "input",
             name: "username",
@@ -40,7 +39,7 @@ const writeFileAsync = util.promisify(fs.writeFile);
             ]
         },
         {   type: "input",
-            name: "dependencies",
+            name: "installations",
             message: "What command should be run to install dependencies?"
         },
         {
@@ -57,5 +56,18 @@ const writeFileAsync = util.promisify(fs.writeFile);
             name: "contribution",
             message: "what does the user need to know about contributing to the repo?"
         }
-    ]);
-//}
+]
+
+function writeToFile("README.md", data)
+    
+
+
+
+
+
+
+inquirer
+        .prompt(questions).then(response => {
+
+        
+        })
